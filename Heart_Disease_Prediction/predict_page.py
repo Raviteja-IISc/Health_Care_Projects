2f291a8ff450c3254ca42998f4bc8f6664ec1e70
Raviteja-IISc <149796640+Raviteja-IISc@users.noreply.github.com>
2024-10-03 23:07:43 +0800

    Update predict_page.py

--- Heart_Disease_Prediction/predict_page.py
+++ Heart_Disease_Prediction/predict_page.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 def load_model():
-    with open('saved_steps.pkl','rb') as file:
+    with open('Heart_Disease_Prediction/saved_steps.pkl','rb') as file:
         data=pickle.load(file)
         #data=joblib.load('Streamlit/saved_steps.pkl')
     return data
